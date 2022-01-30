a03f51e90a5cb22ab02f5778e9167b8da8ffa52c
pcampos <pablo.campos@galilea.cl>
2022-01-30 20:09:33 -0300

    stage

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -8,26 +8,23 @@ def call(){
   
   pipeline {
     agent any
-    parameters {
-        choice choices: ['gradle', 'maven'], description: 'Indicar herramienta de construcciÃ³n', name: 'buildTool'
-    }   
-    environment{
-        STAGE = ''
-            
+    environment {
+       STAGE = ''
     }
+    parameters {
+        choice choices: ['gradle', 'maven'], description: 'Indicar herramienta de construccion', name: 'buildTool'
+    }   
+
     stages {
         stage('Pipeline') {
             steps {
                 script {
-                        println('Pipeline')
-                        println params.buildTool
+                     
                         if (params.buildTool == 'gradle') {
-                            def ejecucion = load 'gradle.groovy'
-                             ejecucion.call()
+                            gradle.call()
                         }
                         else{
-                            def ejecucion = load 'maven.groovy'
-                            ejecucion.call()
+                            maven.call()
                         }     
                 }
             }
